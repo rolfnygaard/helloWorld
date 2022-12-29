d1e9c60fe1c15783fb7e634029d702fbc0794b2e
rolfny <rny@genus.biz>
2022-12-29 16:30:08 +0100

    added multiply by two file

---
+++ hello.py
@@ -0,0 +1,17 @@
+import argparse
+
+def multiply_by_two(x:float):
+
+    return x*2
+
+def get_args():
+    parser = argparse.ArgumentParser(description="doesn't matter")
+    parser.add_argument('number', type=float, default=0)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+    a_number = args.number
+    result = multiply_by_two(a_number)
+    print(f"{a_number} * 2 = {result}")
